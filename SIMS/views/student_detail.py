0a8dd17d9fe6a8c497857353ff7c43fd700e44da
ryokato <aaaz>
2024-02-16 17:45:12 +0900

    aa

--- SIMS/views/student_detail.py
+++ SIMS/views/student_detail.py
@@ -1,5 +1,5 @@
 from os import name
-from flask import Blueprint,app, render_template, request
+from flask import Blueprint,app, render_template
 from flask_login import login_required
 from SIMS import app
 from SIMS.models import School, Student
@@ -8,11 +8,11 @@ bp = Blueprint('student_detail', __name__)
 
 
 # パスパラメータを使う(idの部分を変数に入れる)
-@bp.route('/<str:school>/student_detail/<int:id>',methods=['GET', 'POST'])
+@bp.route('/<school>/student_detail/<int:id>',methods=['GET', 'POST'])
 @login_required
 def student_detail(id,school):
     student = Student.get_one(id=id)
-    school = School.get_one(school=school)
+    school = School.get_one(name=school)
     return render_template('student_detail.html',\
         id = student['id'],\
         name = student['name'],\
@@ -24,7 +24,8 @@ def student_detail(id,school):
         phone = student['phone'],\
         email = student['email'],\
         gmail = student['gmail'],\
-        note = student['note']
+        note = student['note'],\
+        school = school
     )   
 
 
