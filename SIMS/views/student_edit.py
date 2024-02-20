59aa7f5bc7e3688752d91300475ff067582999fe
ryokato <aaaz>
2024-02-20 22:07:59 +0900

    aaa

--- SIMS/views/student_edit.py
+++ SIMS/views/student_edit.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from os import name
-from flask import Blueprint,app, render_template, request
+from flask import Blueprint,app, jsonify, redirect, render_template, request
 from flask_login import login_required
 from httpx import delete
 from SIMS import app
-from SIMS.models import School, Student
+from SIMS.models import School, Student, Class
 
 bp = Blueprint('student_edit', __name__)
 
@@ -12,11 +13,43 @@ bp = Blueprint('student_edit', __name__)
 @login_required
 
 def student_edit(id,school):
+    student = Student.get_one(id=id)
+    schools = School.get_all()
+    class_member = Class.get_all()
+    if request.method == 'POST':
+        student_update = Student.update(
+                        id=request.form.get('id'),
+                        name=request.form.get('name'),
+                        name_kana=request.form.get('name_kana'),
+                        school=school,
+                        class_name=request.form.get('class_name'),
+                        gender=int(request.form.get('gender')),                        
+                        birthday=datetime.strptime(request.form.get('birthday'), '%Y-%m-%d'), # 文字列を日付型に変換
+                        address=request.form.get('address'),
+                        phone=request.form.get('phone'),
+                        email=request.form.get('email'),
+                        gmail=request.form.get('gmail'),
+                        note=request.form.get('note'),
+                                         )
+        return render_template("student_list.html",school=school)
+    
+    elif request.method == 'GET':
+        return render_template('student_edit.html',\
+            student = student,\
+            school = school,\
+            schools = schools,\
+            class_member = class_member
+    )  
+
+
+@bp.route('/<school>/student_delete/<int:id>',methods=['GET', 'POST'])
+@login_required
+
+def student_delete(id,school):
 
     student = Student.get_one(id=id)
     schools = School.get_all()
-    student_delete = Student.delete(id=id)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-    print(student_delete)
+    student_delete =Student.delete(id)
 
     return render_template('student_edit.html',\
         student = student,\
