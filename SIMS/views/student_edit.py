370cbf1536851db8d0736f4a9a1540551677d945
ryokato <aaaz>
2024-02-20 19:06:25 +0900

    aa

--- SIMS/views/student_edit.py
+++ SIMS/views/student_edit.py
@@ -1,6 +1,7 @@
 from os import name
-from flask import Blueprint,app, render_template
+from flask import Blueprint,app, render_template, request
 from flask_login import login_required
+from httpx import delete
 from SIMS import app
 from SIMS.models import School, Student
 
@@ -14,12 +15,16 @@ def student_edit(id,school):
 
     student = Student.get_one(id=id)
     schools = School.get_all()
+    student_delete = Student.delete(id=id)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    print(student_delete)
 
     return render_template('student_edit.html',\
         student = student,\
         school = school,\
-        schools = schools
+        schools = schools,\
+        student_delete = student_delete
     )  
+
 
 if __name__ == '__main__':
     app.debug = True
